ca89ae43c04cf5408f957ba632a3d749154a5938
LiuSihao <liusihao123@qq.com>
2012-11-05 09:56:56 +0800

    delete plugins

--- app/models/hotel.rb
+++
@@ -1,8 +0,0 @@
-class Hotel < ActiveRecord::Base
-  has_many :business_days
-  has_many :room_types
-
-  validates :name, :presence => true
-  validates :location, :presence =>  true
-
-end
--- vendor/extensions/articles/spec/support/factories/refinery/articles.rb
+++
@@ -1,7 +0,0 @@
-
-FactoryGirl.define do
-  factory :article, :class => Refinery::Articles::Article do
-    sequence(:title) { |n| "refinery#{n}" }
-  end
-end
-
